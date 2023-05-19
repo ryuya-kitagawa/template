438455f280d0153102104f1137072da8e4f99497
*lRatHdaW8Y <Ryuya_Kitagawa@itcenex.com>
2023-05-19 18:36:11 +0900

    save

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,24 @@
 $(function () {
   new ScrollHint('.js-scrollable');
+
+  $(".c_ham_menu").click(function () {
+    $(this).toggleClass('active');
+    $(".c_ham_menu_window").toggleClass('active');
+    $(".override_bg").fadeToggle();
+  });
+
+  var state = false;
+  var pos;
+  $(".c_ham_menu").click(function () {
+    if (state == false) {
+      pos = $(window).scrollTop();
+      $("body").addClass("fixed").css({ "top": - pos });
+      state = true;
+    } else {
+      $("body").removeClass("fixed").css({ "top": 0 });
+      window.scrollTo(0, pos);
+      state = false;
+    }
+  });
+
 });
